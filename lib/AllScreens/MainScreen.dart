a6af0267987084b230186f48754fa928f12a11af
marvins56 <82571414+marvins56@users.noreply.github.com>
2022-11-28 13:23:02 +0300

    location update

--- lib/AllScreens/MainScreen.dart
+++ lib/AllScreens/MainScreen.dart
@@ -186,148 +186,148 @@ class _MainScreenState extends State<MainScreen> {
           //   ),
           // ),
 
-          Positioned(
-            left: 0.0,
-            right: 0.0,
-            bottom: 0.0,
-            child: Container(
-              height: 300.0,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(18.0),
-                  topRight: Radius.circular(18.0),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black,
-                    blurRadius: 16.0,
-                    spreadRadius: 0.5,
-                    offset: Offset(0.7, 0.7),
-                  ),
-                ],
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 24.0, vertical: 18.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: 6.0,
-                    ),
-                    Text(
-                      "hey there",
-                      style: TextStyle(fontSize: 12.0),
-                    ),
-                    Text(
-                      "Where To",
-                      style:
-                          TextStyle(fontSize: 20.0, fontFamily: "Brand Bold"),
-                    ),
-                    SizedBox(
-                      height: 6.0,
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(
-                          5.0,
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black54,
-                            blurRadius: 6.0,
-                            spreadRadius: 0.5,
-                            offset: Offset(0.7, 0.7),
-                          ),
-                        ],
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Row(
-                          children: [
-                            Icon(
-                              Icons.search,
-                              color: Colors.blueAccent,
-                            ),
-                            SizedBox(
-                              height: 10.0,
-                            ),
-                            Text("Search destination .."),
-                          ],
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 24.0,
-                    ),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.work,
-                          color: Colors.grey,
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Add Work ",
-                            ),
-                            SizedBox(
-                              height: 14.0,
-                            ),
-                            Text(
-                              "Add Work address",
-                              style: TextStyle(
-                                  color: Colors.black54, fontSize: 12.0),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    DividerWidget(),
-                    SizedBox(
-                      height: 16.0,
-                    ),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.home,
-                          color: Colors.grey,
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Add Home ",
-                            ),
-                            SizedBox(
-                              height: 14.0,
-                            ),
-                            Text(
-                              "Add Work address",
-                              style: TextStyle(
-                                  color: Colors.black54, fontSize: 12.0),
-                            ),
-                          ],
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ),
+          // Positioned(
+          //   left: 0.0,
+          //   right: 0.0,
+          //   bottom: 0.0,
+          //   child: Container(
+          //     height: 300.0,
+          //     decoration: BoxDecoration(
+          //       color: Colors.white,
+          //       borderRadius: BorderRadius.only(
+          //         topLeft: Radius.circular(18.0),
+          //         topRight: Radius.circular(18.0),
+          //       ),
+          //       boxShadow: [
+          //         BoxShadow(
+          //           color: Colors.black,
+          //           blurRadius: 16.0,
+          //           spreadRadius: 0.5,
+          //           offset: Offset(0.7, 0.7),
+          //         ),
+          //       ],
+          //     ),
+          //     child: Padding(
+          //       padding: const EdgeInsets.symmetric(
+          //           horizontal: 24.0, vertical: 18.0),
+          //       child: Column(
+          //         crossAxisAlignment: CrossAxisAlignment.start,
+          //         children: [
+          //           SizedBox(
+          //             height: 6.0,
+          //           ),
+          //           Text(
+          //             "hey there",
+          //             style: TextStyle(fontSize: 12.0),
+          //           ),
+          //           Text(
+          //             "Where To",
+          //             style:
+          //                 TextStyle(fontSize: 20.0, fontFamily: "Brand Bold"),
+          //           ),
+          //           SizedBox(
+          //             height: 6.0,
+          //           ),
+          //           Container(
+          //             decoration: BoxDecoration(
+          //               color: Colors.white,
+          //               borderRadius: BorderRadius.circular(
+          //                 5.0,
+          //               ),
+          //               boxShadow: [
+          //                 BoxShadow(
+          //                   color: Colors.black54,
+          //                   blurRadius: 6.0,
+          //                   spreadRadius: 0.5,
+          //                   offset: Offset(0.7, 0.7),
+          //                 ),
+          //               ],
+          //             ),
+          //             child: Padding(
+          //               padding: const EdgeInsets.all(12.0),
+          //               child: Row(
+          //                 children: [
+          //                   Icon(
+          //                     Icons.search,
+          //                     color: Colors.blueAccent,
+          //                   ),
+          //                   SizedBox(
+          //                     height: 10.0,
+          //                   ),
+          //                   Text("Search destination .."),
+          //                 ],
+          //               ),
+          //             ),
+          //           ),
+          //           SizedBox(
+          //             height: 24.0,
+          //           ),
+          //           Row(
+          //             children: [
+          //               Icon(
+          //                 Icons.work,
+          //                 color: Colors.grey,
+          //               ),
+          //               SizedBox(
+          //                 height: 10.0,
+          //               ),
+          //               Column(
+          //                 crossAxisAlignment: CrossAxisAlignment.start,
+          //                 children: [
+          //                   Text(
+          //                     "Add Work ",
+          //                   ),
+          //                   SizedBox(
+          //                     height: 14.0,
+          //                   ),
+          //                   Text(
+          //                     "Add Work address",
+          //                     style: TextStyle(
+          //                         color: Colors.black54, fontSize: 12.0),
+          //                   ),
+          //                 ],
+          //               ),
+          //             ],
+          //           ),
+          //           SizedBox(
+          //             height: 10.0,
+          //           ),
+          //           DividerWidget(),
+          //           SizedBox(
+          //             height: 16.0,
+          //           ),
+          //           Row(
+          //             children: [
+          //               Icon(
+          //                 Icons.home,
+          //                 color: Colors.grey,
+          //               ),
+          //               SizedBox(
+          //                 height: 10.0,
+          //               ),
+          //               Column(
+          //                 crossAxisAlignment: CrossAxisAlignment.start,
+          //                 children: [
+          //                   Text(
+          //                     "Add Home ",
+          //                   ),
+          //                   SizedBox(
+          //                     height: 14.0,
+          //                   ),
+          //                   Text(
+          //                     "Add Work address",
+          //                     style: TextStyle(
+          //                         color: Colors.black54, fontSize: 12.0),
+          //                   ),
+          //                 ],
+          //               ),
+          //             ],
+          //           )
+          //         ],
+          //       ),
+          //     ),
+          //   ),
+          // ),
         ],
       ),
     );
